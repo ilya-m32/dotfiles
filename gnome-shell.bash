d9ef609b816bb30434ffefde4cbe934f0ae28630
Ilya Malyavin <mail@ilya-m.com>
2025-04-09 20:26:21 +0200

    feat: add more gnome-shell hotkeys

--- gnome-shell.bash
+++ gnome-shell.bash
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Workspaces
 for i in {1..7}
 do
     gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "[\"<Alt>$i\"]"
-
     gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$i "[\"<Shift><Alt>$i\"]"
 done
 
 gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "[\"<Shift><Control><Alt>f\"]"
 gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "[\"<Control><Alt>f\"]"
+gsettings set org.gnome.desktop.wm.keybindings close "[\"<Primary><Alt>q\"]"
+gsettings set org.gnome.shell.keybindings toggle-application-view "[\"<Alt>r\"]"
+
