f5e1786f0f42812d11c8eaee32053e82d2fd0fe3
Ilya Malyavin <mail@ilya-m.com>
2025-06-28 19:28:35 +0200

    system gl support

---
+++ etc/nix/flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    system-manager = {
+      url = "github:numtide/system-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-system-graphics = {
+      url = "github:soupglasses/nix-system-graphics";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, flake-utils, nixpkgs, system-manager, nix-system-graphics }: {
+    systemConfigs.default = system-manager.lib.makeSystemConfig {
+      modules = [
+        nix-system-graphics.systemModules.default
+        ({
+          config = {
+            nixpkgs.hostPlatform = "x86_64-linux";
+            system-manager.allowAnyDistro = true;
+            system-graphics.enable = true;
+          };
+        })
+      ];
+    };
+  };
+}
